6eb583f9c5ba9999fa52548f125db74d430d1f46
kierquebs <kierquebs@gmail.com>
2023-05-12 17:21:15 +0800

    create unit testing for CreateAccount function

---
+++ db/sqlc/account_test.go
@@ -0,0 +1,35 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/kierquebs/simplebank.kierquebral.com/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomAccount(t *testing.T) Account {
+	arg := CreateAccountParams{
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+
+	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreatAccount(t *testing.T) {
+	createRandomAccount(t)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/kierquebs/simplebank.kierquebral.com
 
 go 1.20
+
+require github.com/lib/pq v1.10.9
+
+require (
+	github.com/bxcodec/faker/v4 v4.0.0-beta.3 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
