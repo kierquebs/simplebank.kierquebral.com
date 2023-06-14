d7bca173952c9c0c20d461c2506e9a8d9795cb82
kierquebs <kierquebs@gmail.com>
2023-06-14 10:17:10 +0800

    remove "foreign_key_violation" pqError in switch case statement

--- api/user.go
+++ api/user.go
@@ -44,7 +44,7 @@ func (server *Server) createUser(ctx *gin.Context) {
 	if err != nil {
 		if pqErr, ok := err.(*pq.Error); ok {
 			switch pqErr.Code.Name() {
-			case "foreign_key_violation", "unique_violation":
+			case "unique_violation":
 				ctx.JSON(http.StatusForbidden, errorResponse(err))
 				return
 			}
