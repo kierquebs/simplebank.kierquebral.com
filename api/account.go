77d3f0e36d58acbd69031c02e5c7cdf0921bcf64
kierquebs <kierquebs@gmail.com>
2023-06-15 15:39:19 +0800

    logged-in user can only list accounts that belong to him/her

--- api/account.go
+++ api/account.go
@@ -88,7 +88,9 @@ func (server *Server) ListAccount(ctx *gin.Context) {
 		return
 	}
 
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
 	arg := db.ListAccountsParams{
+		Owner:  authPayload.Username,
 		Limit:  req.PageSize,
 		Offset: (req.PageID - 1) * req.PageSize,
 	}
