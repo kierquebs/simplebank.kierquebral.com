f40a83558a4f965c27335267c383cbb2a0cb8663
kierquebs <40307377+kierquebs@users.noreply.github.com>
2023-06-15 16:40:34 +0800

    Implement authentication middleware and  authorization rules (#2)
    * init commit for creating a middleware
    
    * init commit for unit testing of middleware
    
    * implements middleware on routes
    
    * logged-in user can only create an account for him/herself
    
    * logged-in user can only get accounts that he/she owns
    
    * logged-in user can only list accounts that belong to him/her
    
    * logged-in user can only send money from his/her own account
    
    * implement authorization in TestGetAccountAPI function
    
    * create unit testing for Create Account API with authentication
    
    * create unit test for List Account API with authentication
    
    * add test case for UnauthorizedUser and NoAuthorization
    
    * implements authorization setup in transfer api

--- api/account.go
+++ api/account.go
@@ -2,15 +2,16 @@ package api
 
 import (
 	"database/sql"
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	db "github.com/kierquebs/simplebank.kierquebral.com/db/sqlc"
+	"github.com/kierquebs/simplebank.kierquebral.com/token"
 	"github.com/lib/pq"
 )
 
 type createAccountRequest struct {
-	Owner    string `json:"owner" binding:"required"`
 	Currency string `json:"currency" binding:"required,currency"`
 }
 
@@ -21,8 +22,9 @@ func (server *Server) createAccount(ctx *gin.Context) {
 		return
 	}
 
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
 	arg := db.CreateAccountParams{
-		Owner:    req.Owner,
+		Owner:    authPayload.Username,
 		Currency: req.Currency,
 		Balance:  0,
 	}
@@ -64,6 +66,13 @@ func (server *Server) getAccount(ctx *gin.Context) {
 		return
 	}
 
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+	if account.Owner != authPayload.Username {
+		err := errors.New("account doesn't belong to the authenticated user")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+
 	ctx.JSON(http.StatusOK, account)
 }
 
@@ -79,7 +88,9 @@ func (server *Server) ListAccount(ctx *gin.Context) {
 		return
 	}
 
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
 	arg := db.ListAccountsParams{
+		Owner:  authPayload.Username,
 		Limit:  req.PageSize,
 		Offset: (req.PageID - 1) * req.PageSize,
 	}
