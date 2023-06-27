fa1394812e10461a5cdab1502cfcba256de16faa
kierquebs <kierquebs@gmail.com>
2023-06-27 17:16:28 +0800

    seventh, implement the generated code of protobuf

---
+++ gapi/converter.go
@@ -0,0 +1,19 @@
+package gapi
+
+import (
+	db "github.com/kierquebs/simplebank.kierquebral.com/db/sqlc"
+	"github.com/kierquebs/simplebank.kierquebral.com/pb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func convertUser(user db.User) *pb.User {
+	return &pb.User{
+		Username:          user.Username,
+		FirstName:         user.FirstName,
+		MiddleName:        user.MiddleName,
+		LastName:          user.LastName,
+		Email:             user.Email,
+		PasswordChangedAt: timestamppb.New(user.PasswordChangedAt),
+		CreatedAt:         timestamppb.New(user.CreatedAt),
+	}
+}
---
+++ gapi/rpc_login_user.go
@@ -0,0 +1,76 @@
+package gapi
+
+import (
+	"context"
+	"errors"
+
+	db "github.com/kierquebs/simplebank.kierquebral.com/db/sqlc"
+	"github.com/kierquebs/simplebank.kierquebral.com/pb"
+	"github.com/kierquebs/simplebank.kierquebral.com/util"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	user, err := server.store.GetUser(ctx, req.Username)
+	if err != nil {
+		if errors.Is(err, db.ErrRecordNotFound) {
+			return nil, status.Errorf(codes.NotFound, "user not found")
+
+		}
+		return nil, status.Errorf(codes.Internal, "failed get user")
+	}
+
+	// THIRD: Validate if the supplied password from JSON Request is same as
+	// the returned hashed password from DB
+
+	err = util.CheckPassword(req.Password, user.HashedPassword)
+	if err != nil {
+		return nil, status.Errorf(codes.PermissionDenied, "incorrect password")
+	}
+
+	accessToken, accessPayload, err := server.tokenMaker.CreateToken(
+		user.Username,
+		server.config.AccessTokenDuration,
+	)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to create access token")
+	}
+
+	refreshToken, refreshPayload, err := server.tokenMaker.CreateToken(
+		user.Username,
+		server.config.RefreshTokenDuration,
+	)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to create refresh token")
+	}
+
+	mtdt := server.extractMetadata(ctx)
+
+	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
+		ID:           refreshPayload.ID,
+		Username:     user.Username,
+		RefreshToken: refreshToken,
+		UserAgent:    mtdt.UserAgent,
+		ClientIp:     mtdt.ClientIP,
+		IsBlocked:    false,
+		ExpiresAt:    refreshPayload.ExpiredAt,
+	})
+
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to create session")
+
+	}
+
+	rsp := &pb.LoginUserResponse{
+		SessionId:             session.ID.String(),
+		AccessToken:           accessToken,
+		AccessTokenExpiresAt:  timestamppb.New(accessPayload.ExpiredAt),
+		RefreshToken:          refreshToken,
+		RefreshTokenExpiresAt: timestamppb.New(refreshPayload.ExpiredAt),
+		User:                  convertUser(user),
+	}
+
+	return rsp, nil
+}
