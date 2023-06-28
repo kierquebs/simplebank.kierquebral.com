5bedbb8796790af3817474261c0891ea3a57982c
kierquebs <kierquebs@gmail.com>
2023-06-28 17:31:52 +0800

    Emebeding of the generated statik file to the go binary.

--- main.go
+++ main.go
@@ -10,10 +10,12 @@ import (
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/kierquebs/simplebank.kierquebral.com/api"
 	db "github.com/kierquebs/simplebank.kierquebral.com/db/sqlc"
+	_ "github.com/kierquebs/simplebank.kierquebral.com/doc/statik"
 	"github.com/kierquebs/simplebank.kierquebral.com/gapi"
 	"github.com/kierquebs/simplebank.kierquebral.com/pb"
 	"github.com/kierquebs/simplebank.kierquebral.com/util"
 	_ "github.com/lib/pq"
+	"github.com/rakyll/statik/fs"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/encoding/protojson"
@@ -86,12 +88,17 @@ func runGatewayServer(config util.Config, store db.Store) {
 	mux := http.NewServeMux()
 	mux.Handle("/", grpcMux)
 
-	fs := http.FileServer(http.Dir("./doc/swagger"))
-	mux.Handle("/swagger/", http.StripPrefix("/swagger/", fs))
+	statikFS, err := fs.New()
+	if err != nil {
+		log.Fatal("cannot create statik fs")
+	}
+
+	swaggerHAndler := http.StripPrefix("/swagger/", http.FileServer(statikFS))
+	mux.Handle("/swagger/", swaggerHAndler)
 
 	listener, err := net.Listen("tcp", config.GatewayServerAddress)
 	if err != nil {
-		log.Fatal("cannot create listener")
+		log.Fatal("cannot create listener:", err)
 	}
 
 	log.Printf("start HTTP gateway server at %s", listener.Addr().String())
