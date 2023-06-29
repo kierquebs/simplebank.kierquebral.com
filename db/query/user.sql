b1e05cc9cf812f5ffb64d25e20cc4dfd843c46e7
kierquebs <kierquebs@gmail.com>
2023-06-29 14:41:25 +0800

    add update query that accept nullable value in all fields except for username

--- db/query/user.sql
+++ db/query/user.sql
@@ -13,3 +13,15 @@ INSERT INTO users (
 -- name: GetUser :one
 SELECT * FROM users
 WHERE username = $1 LIMIT 1;
+
+-- name: UpdateUser :one
+UPDATE users
+SET
+    hashed_password = COALESCE(sqlc.narg(hashed_password), hashed_password),
+    first_name = COALESCE(sqlc.narg(first_name), first_name),
+    middle_name = COALESCE(sqlc.narg(middle_name), middle_name),
+    last_name = COALESCE(sqlc.narg(last_name), last_name),
+    email = COALESCE(sqlc.narg(email), email)
+WHERE
+    username = sqlc.arg(username)
+RETURNING *;
