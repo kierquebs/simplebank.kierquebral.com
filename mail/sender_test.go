119a2c937849c35543bc4b78d51a4933dc2d193b
kierquebs <kierquebs@gmail.com>
2023-07-04 17:34:28 +0800

    create unit test for sending email

---
+++ mail/sender_test.go
@@ -0,0 +1,30 @@
+package mail
+
+import (
+	"testing"
+
+	"github.com/kierquebs/simplebank.kierquebral.com/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSendEmailWithGmail(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	config, err := util.LoadConfig("..")
+	require.NoError(t, err)
+
+	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+
+	subject := "A test email"
+	content := `
+	<h1>Hello world</h1>
+	<p>This is a test message from Kier Quebral</p>
+	`
+	to := []string{"kierquebs@gmail.com"}
+	attachFiles := []string{"../README.md"}
+
+	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+	require.NoError(t, err)
+}
