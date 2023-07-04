91977de64a89b883d4d483e75444bc410eb67a27
kierquebs <kierquebs@gmail.com>
2023-07-04 11:50:55 +0800

    implement task payload

--- gapi/rpc_create_user.go
+++ gapi/rpc_create_user.go
@@ -2,11 +2,14 @@ package gapi
 
 import (
 	"context"
+	"time"
 
+	"github.com/hibiken/asynq"
 	db "github.com/kierquebs/simplebank.kierquebral.com/db/sqlc"
 	"github.com/kierquebs/simplebank.kierquebral.com/pb"
 	"github.com/kierquebs/simplebank.kierquebral.com/util"
 	"github.com/kierquebs/simplebank.kierquebral.com/val"
+	"github.com/kierquebs/simplebank.kierquebral.com/worker"
 	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -39,6 +42,19 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 			return nil, status.Errorf(codes.AlreadyExists, "username already exists: %s", err)
 		}
 		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err)
+	}
+
+	taskPayload := &worker.PayloadSendVerifyEmail{
+		Username: user.Username,
+	}
+	opts := []asynq.Option{
+		asynq.MaxRetry(10),
+		asynq.ProcessIn(10 * time.Second),
+		asynq.Queue(worker.QueueCritical),
+	}
+	err = server.taskDistributor.DistributeTaskSendVerifyEmail(ctx, taskPayload, opts...)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to distribute task to send verify email")
 	}
 
 	resp := &pb.CreateUserResponse{
