7d48ec655fad2d6bd7ccca2f49948a04682f909a
kierquebs <kierquebs@gmail.com>
2023-07-04 11:51:49 +0800

    Add redis addres

--- util/config.go
+++ util/config.go
@@ -14,6 +14,7 @@ type Config struct {
 	HTTPServerAddress    string        `mapstructure:"HTTP_SERVER_ADDRESS"`
 	GRPCServerAddress    string        `mapstructure:"GRPC_SERVER_ADDRESS"`
 	GatewayServerAddress string        `mapstructure:"GATEWAY_SERVER_ADDRESS"`
+	RedisAddress         string        `mapstructure:"REDIS_ADDRESS"`
 	TokenSymmetricKey    string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
 	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
 	RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
